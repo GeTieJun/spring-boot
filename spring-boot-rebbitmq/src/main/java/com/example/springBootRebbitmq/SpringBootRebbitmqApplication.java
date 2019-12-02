c81d817c05333153221a8dcaabb3751db7f13ea4
tiejun.ge <1354507073@qq.com>
2019-12-02 08:47:11 +0800

    Add: add rebbitMq

---
+++ spring-boot-rebbitmq/src/main/java/com/example/springBootRebbitmq/SpringBootRebbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootRebbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRebbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRebbitmqApplication.class, args);
+	}
+
+}
---
+++ spring-boot-rebbitmq/src/test/java/com/example/springBootRebbitmq/SpringBootRebbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springBootRebbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRebbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
